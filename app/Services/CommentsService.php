2fc523fe1841549e8ead113efd42cd12229070ce
Ariana <ariana.crvenka@gmail.com>
2017-12-06 21:03:34 +0100

    counting comments

--- app/Services/CommentsService.php
+++ app/Services/CommentsService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use Illuminate\View\Factory;
 use Sentinel;
 use App\Models\Post;
+use App\Models\Comment;
 
 class CommentsService
 {
@@ -17,10 +18,17 @@ class CommentsService
 	
 	public function pendingComments()
 	{
-		$user_id = Sentinel::getUser()->id;
+		/* $user_id = Sentinel::getUser()->id;
 		
 		$posts = Post::where('user_id', $user_id)->count();
 		
-		return $posts;
+		return $posts; */
+		
+		$user_id = Sentinel::getUser()->id;
+		
+		$comments = Comment::where('user_id', $user_id)->where('status', 0)->count();
+		
+		return $comments;
+		
 	}	
 }
